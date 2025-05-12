04b7138be85da7d8d8bfdbb60596bd8deb6d57fa
yuricifiello <yuricifiello@gmail.com>
2025-05-12 16:22:18 +0200

    add asset statics

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.static("immagini"));
+
 app.get(`/`, (req, res) => {
   res.json("Server del mio blog");
 });
